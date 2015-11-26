d8fe8dcf04ff4787393482cf27657d008ec113b3
jmexe <ming_jia@student.uml.edu>
2015-11-25 23:16:20 -0500

    Update CounterManager.java

--- src/com/jmexe/util/CounterManager.java
+++ src/com/jmexe/util/CounterManager.java
@@ -6,9 +6,9 @@ import java.sql.*;
  * Created by Jmexe on 10/10/14.
  */
 public class CounterManager {
-    private static String db_url = "jdbc:mysql://localhost:3306/util";
-    private static String user = "root";
-    private static String password = "wtsql2014";
+    private static String db_url = "";
+    private static String user = "";
+    private static String password = "";
 
     public static void addRecord(long ts, String url, int type, String msg, String text, String summary) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
